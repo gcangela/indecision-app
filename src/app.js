d185702cb2e6ba1dd75468edcf20be4db31c6421
gcangela <gcangela90@gmail.com>
2017-10-09 21:22:59 +0200

    used javascript expressions to render text

---
+++ src/app.js
@@ -0,0 +1,37 @@
+console.log('App.js is running!')
+
+// JSX - JavaScript XML 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'This is some info'
+}
+
+var template= (
+  <div>
+      <h1>{app.title}</h1>
+      <p>{app.subtitle}</p>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+  </div>
+  )
+
+var user = {
+  name: 'GC',
+  age: 27,
+  location: 'Amsterdam'
+}  
+
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+)  
+
+var appRoot = document.getElementById('app')
+
+
+ReactDOM.render(template, appRoot)
