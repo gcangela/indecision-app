45ee8bd18c59500ad9f5fefe6380d7334960bdad
gcangela <gcangela90@gmail.com>
2017-11-05 17:23:20 +0100

    Broke down all individual components into seperate files

--- src/app.js
+++ src/app.js
@@ -1,22 +1,5 @@
-// // import './utils.js'
-// import subtract, { square, add } from './utils.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IndecisionApp from './components/indecisionApp'
 
-// console.log('app.js is running')
-// console.log(square(4))
-// console.log(add(2,3))
-// console.log(subtract(10, 5))
-
-//person.js 
-//named export isAdult(18) - true if adult otherwise false 
-//named export canDrink(18) - true if 21 and over otherwise false 
-
-import isSenior, { canDrink, isAdult } from './person.js' 
-
-console.log(isAdult(12))
-console.log(canDrink(21))
-console.log(isSenior(65))
-
-// setting up the default export and function 
-//grab the default and call it 
-
-
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
